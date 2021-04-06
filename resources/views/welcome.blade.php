c019d7cc5d422d4dc88dfb13b009200c3dc197ae
ballardo <ballardo.lopez123@gmail.com>
2021-04-06 02:03:27 -0500

    icono

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>ARwax</title>
 
+        <!-- Icono -->
+        <link rel="shortcut icon" href="{{asset('public/img/logo.ico')}}" />
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         
