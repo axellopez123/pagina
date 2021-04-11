2f3dfded3f5f95a43bd84fb68e11c493808db16b
ballardo <ballardo.lopez123@gmail.com>
2021-04-11 13:26:32 -0500

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -191,10 +191,17 @@
     </div>
     <div class="container">
         <div class="row align-items-end bg-secondary">
-
+semi-footer
         </div>
     </div>
     </body>
+    <footer>
+    <div class="container">
+        <div class="row align-items-start bg-secondary">
+                Footer
+        </div>
+    </div>
+    </footer>
     <!-- SCRIPTS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </html>
